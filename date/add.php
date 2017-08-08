e7ec2128ade03f5088690993f90e15e159862ff2
Silviu Georgescu <georgescuslv@gmail.com>
2017-08-08 21:13:36 +0300

    08-08-2017

--- date/add.php
+++ date/add.php
@@ -5,11 +5,30 @@
 <body>
 
 <?php
+
+/**
+ * create new number (auto increment)
+ * 1. read from last.contact.id
+ * 2. +1 (increment)
+ * 3. use of the last incremented number
+ * 4. save it in last.contact.id
+ * @return int
+ */
+function getNextId(){
+    $idString = file_get_contents("last.contact.id");
+    $id = intval($idString);
+    $id++;
+    file_put_contents("last.contact.id", $id);
+    return $id;
+}
+
 $contentString = file_get_contents("contacte.json");
 $contacte = json_decode($contentString, true);
 
+$id = getNextid();
+
 $newPerson = array(
-"id" => 5, // to do create new number (auto increment)
+"id" => $id, // to do create new number (auto increment)
 "firstName" => $_GET["firstName"],
 "lastName" => $_GET["lastName"],
 "phone" =>  $_GET["phone"]
@@ -18,8 +37,9 @@ $newPerson = array(
 $contacte[] = $newPerson; //add new person into array
 
 $contentString = json_encode($contacte, true);
-file_put_contents("contacte.json", $contentString);
+file_put_contents("contacte.json", $contentString);    // salvare fisier
 
+header('Location: ../contacte.html');
 ?>
 
 contact added...
