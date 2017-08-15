4ea956aed1810af1bf1fadaeb528f599e4dd1145
Silviu Georgescu <georgescuslv@gmail.com>
2017-08-15 19:57:35 +0300

    15-08-2017 edit remove

--- js/contacte.js
+++ js/contacte.js
@@ -5,8 +5,8 @@ function getRow(contact) {
     var firstName = contact.firstName || '';
     var row = '<tr><td>' + lastName + '</td><td>' + firstName + '</td><td>' + phone + '</td>' +
         '<td class="actions">'+
-        '<span><a href="date/remove.html?id=' + id + '">&#x2716;</a></span> '+
-        '<span><a href="#">&#x270E;</a></span>'+
+        '<span><a href="date/remove.php?id=' + id + '">&#x2716;</a></span> '+
+        '<span><a href="#" class="edit" data-id="' + id + '">&#x270E;</a></span>'+
         '</td>' +
         '</tr>';
     return row;
@@ -18,17 +18,29 @@ function createRow(contact) {
     tableContent += getRow(contact);
 }
 
-// 2. TODO response/content type (json)
-// 3. TODO status codes: 200, 404, 5**, 3**
-$.ajax('date/contacte.json', {cache: false}).done(function (contacte) {
-    console.debug('contacte', contacte);
+$.ajax('date/contacte.json', {
+    cache: false,
+    dataType: 'json'
+}).done(function (contacte) {
+    console.debug('contacts loaded', contacte);
     contacte.forEach(createRow);
     $("#contacts-list tbody").html(tableContent);
+
+    $('#contacts-list a.edit').click(function() {
+        var id = $(this).data('id');
+        var contact = contacte.find(function(c) {
+            return c.id == id;
+        });
+        console.debug('remove', id, contact, this);
+
+        $('input[name=id]').val(contact.id);
+        $('input[name=lastName]').val(contact.lastName);
+        $('input[name=firstName]').val(contact.firstName);
+        $('input[name=phone]').val(contact.phone);
+    });
 });
 
 // 1. convert from array of arrays into json
-// 2. load contacts from json file with AJAX
 // 3. remove contacts (UI)
 // 4. edit contact (UI)
 // 5. TODO php includes / templates
-// 6. TODO create new number (auto increment when add new contact)
